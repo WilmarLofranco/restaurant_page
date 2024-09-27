e58d2f726988fbebece41af57220d28b87b3b743
WilmarLofranco <wilmarlofranco@gmail.com>
2024-09-27 22:33:19 +0800

    Import showHome fn from home.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import "./styles.css";
 import { showMenu } from "./menu.js";
 import { showContact } from "./contact.js";
+import { showHome } from "./home.js";
 
+showHome();
 
 console.log("gwapo ko!");
