a22b54c01e47b62e84e45e1ad30f39bd2c72c66f
WilmarLofranco <wilmarlofranco@gmail.com>
2024-09-27 23:37:03 +0800

    Add text to hero section

--- src/home.js
+++ src/home.js
@@ -1,5 +1,7 @@
+// home.js
 import { content } from "./menu";
 const home = document.querySelector("#home");
+import image from "./imgs/hero.jpg"
 
 const showHome = function () {
     content.innerHTML = "";
@@ -11,11 +13,13 @@ const showHome = function () {
     hero.classList.add("hero");
     homeContainer.appendChild(hero);
     const heroImage = document.createElement("img");
-    heroImage.src = "./imgs/belly.jpg";
-    heroImage.alt = "Hero Image";
-    heroImage.style.width = "80%";
-    heroImage.style.height = "80%";
+    heroImage.src = image;
+    heroImage.style.width = "70%";
+    heroImage.style.height = "70%";
     hero.appendChild(heroImage);
+    const intro = document.createElement("p");
+    intro.textContent = "Welcome to The Krakel Resto-Bar, located in the heart of Manolo Fortich, Bukidnon. We take pride in serving the finest seafood dishes that will leave you craving for more. Join us for an unforgettable dining experience where fresh flavors and quality come together in every bite.";
+    hero.appendChild(intro);
 
     const loc = document.createElement("div");
     loc.classList.add("location");
