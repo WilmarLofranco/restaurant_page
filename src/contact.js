f743cbd7c70b0d67ead239b6fb30d93d31bcada1
WilmarLofranco <wilmarlofranco@gmail.com>
2024-09-28 17:40:43 +0800

    Add contact info, finish showContact fn

--- src/contact.js
+++ src/contact.js
@@ -4,6 +4,14 @@ const contact = document.querySelector("#contact");
 
 const showContact = function () {
     content.innerHTML = "";
+
+    const contactContainer = document.createElement("div");
+    contactContainer.classList.add("contactContainer");
+    contactContainer.style.backgroundImage = "url('./imgs/contact.png')";
+    contactContainer.style.backgroundSize = "cover"; // Change to 'cover' if needed
+    contactContainer.style.width = "100%"; // Set width
+    contactContainer.style.height = "100vh"; // Set heigh
+    content.appendChild(contactContainer);
 }
 
 contact.addEventListener('click', showContact);
--- src/home.js
+++ src/home.js
@@ -1,7 +1,8 @@
 // home.js
 import { content } from "./menu";
 const home = document.querySelector("#home");
-import image from "./imgs/hero.jpg"
+import heroImg from "./imgs/hero.jpg";
+import krakelLoc from "./imgs/krakel.png"
 
 const showHome = function () {
     content.innerHTML = "";
@@ -13,7 +14,7 @@ const showHome = function () {
     hero.classList.add("hero");
     homeContainer.appendChild(hero);
     const heroImage = document.createElement("img");
-    heroImage.src = image;
+    heroImage.src = heroImg;
     heroImage.style.width = "70%";
     heroImage.style.height = "70%";
     hero.appendChild(heroImage);
@@ -24,10 +25,24 @@ const showHome = function () {
     const loc = document.createElement("div");
     loc.classList.add("location");
     homeContainer.appendChild(loc);
+    const textLoc = document.createElement("p");
+    textLoc.innerHTML = "<h3>LOCATION</h1>Upper Pol-oton, Tankulan<br>Manolo Fortich, Bukidnon, 8703<br>House #1463";
+    loc.appendChild(textLoc);
+
+    const krakelLocation = document.createElement("img");
+    krakelLocation.src = krakelLoc;
+    krakelLocation.style.width = "40%";
+    krakelLocation.style.height = "80%";
+    loc.appendChild(krakelLocation);
 
     const hoursOpen = document.createElement("div");
     hoursOpen.classList.add("hoursOpen");
     homeContainer.appendChild(hoursOpen);
+    const openTime = document.createElement("p");
+    openTime.innerHTML = "<strong><em>~Monday - Saturday: 10am - 10pm / Sunday: Closed~</em></strong>";
+    openTime.style.color = "white";
+    openTime.style.textAlign = "center";
+    hoursOpen.appendChild(openTime);
 
     content.appendChild(homeContainer);
 }
